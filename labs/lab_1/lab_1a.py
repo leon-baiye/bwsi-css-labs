df4aabe6db5e6468ca3159763b8d2de8a30a2ad2
Leon Baiye <leon032020@gmail.com>
2026-02-07 22:21:43 -0500

    Add intro to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Leon Baiye" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hello everyone! My name is Leon Baiye. I'm a junior from North Carolina. I serve as class president and organize a few committees at my high school. Outside of that, I'm a trombonist in a local youth orchestra and I like working on ML projects. I'm currently working on a computer vision model for environmentally conscious shopping. I'm not a huge fan of ice cream, but I love sorbet of any flavor. Looking forward to learning with you all!")
 if __name__ == "__main__":
     main()
